ecef20675b6a6b8397a32dcfab4a0e379e1a15b5
pyb <pyb@pyb.me.uk>
2025-02-13 13:53:50 -0500

    Added Load/save calls (persist.ts), currently unused

---
+++ src/app/persist.ts
@@ -0,0 +1,23 @@
+// Saving, Loading
+
+import superjson from 'superjson';
+import { GameState } from "./GameState";
+import { initialGameState } from "./GameData";
+
+const LIKey = "LISAVE";
+
+export const load = ():GameState => {
+    const s = localStorage.getItem(LIKey);
+    if (!s)
+        return initialGameState;
+    else
+    {
+        const state:GameState = superjson.parse(s);
+        return state;
+    }
+}
+
+export const save = (state: GameState):void => {
+    const s:string = superjson.stringify(state);
+    localStorage.setItem(LIKey, s); 
+}
