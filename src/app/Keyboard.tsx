ab6ed45620e4b441694c63452e30d6218352cd47
pyb <pyb@pyb.me.uk>
2025-02-12 23:26:31 -0500

    Keyboard component WIP

--- src/app/Keyboard.tsx
+++ src/app/Keyboard.tsx
@@ -1,31 +1,47 @@
 import styles from "./css/keyboard.module.css"
-import * as Types from "./GameTypes"
+import {KeyStatus, KeyMode} from "./GameTypes"
+import { HStack, StackSeparator, Kbd, Theme, VStack } from "@chakra-ui/react";
 
-interface Props {
-    keyStatus: Map<string, Types.KeyStatus>
+interface KeyProps {
+    text: string,
+    modes: Set<KeyMode>
 };
 
-const Keyboard = ({keyStatus}:Props) => {
+const Key = ({text, modes}:KeyProps) => {
+    return (
+    <div className={styles.key}><Kbd size='lg' colorPalette="orange"> {text} </Kbd></div>
+);}
+
+interface KeyboardProps {
+    keyStatus: Map<string, KeyStatus>
+};
+
+const Keyboard = ({keyStatus}:KeyboardProps) => {
     let unlockedKeys = new Array<string>();
     let availableKeys = new Array<string>();
 
     for (const [key, status] of keyStatus) {
-        if (status.modes.has(Types.KeyMode.Unlocked))
+        if (status.modes.has(KeyMode.Unlocked))
             unlockedKeys.push(key);
-        if (status.modes.has(Types.KeyMode.Available))
+        if (status.modes.has(KeyMode.Available))
             availableKeys.push(key);
     }
 
     return (
-        <div className={styles.keyboardComponent}>
-            <div>{availableKeys.map((key: string) =>
-                <span className={styles.key} key={key}>{key}</span>
-            )}</div>
-            <div>{unlockedKeys.map((key: string) =>
-                <span className={styles.key} key={key}>{key}</span>
-            )}</div>
+        <div className="dark">
+            <div className={styles.keyboardComponent}>
+                <div className={styles.keyRow}>
+                    <>
+                    {unlockedKeys.map((key: string) =>
+                        <Key text={key} key={key} modes={keyStatus.get(key)?.modes as Set<KeyMode>} />)}
+                    {availableKeys.map((key: string) =>
+                        <Key text={key} key={key} modes={keyStatus.get(key)?.modes as Set<KeyMode>} />)}
+                    </>
+                </div>
+            </div>
         </div>
     );
 }
 
+// <span className={styles.key} key={key}>{key}</span>
 export default Keyboard;
